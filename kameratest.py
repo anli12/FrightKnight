a44e61e66392ce4549b4f5947509f80ad742d307
Anna <mail@mail.com>
2021-12-04 13:03:54 +0100

    init

--- kameratest.py
+++ kameratest.py
@@ -2,23 +2,28 @@ import cv2
 import time
 import face_recognition
 
-key = cv2. waitKey(1)
+name="img.jpg"
 webcam = cv2.VideoCapture(0)
 while True:
     try:
         check, frame = webcam.read()
-        print(check) #prints true as long as the webcam is running
-        print(frame) #prints matrix values of each framecd 
-        cv2.imshow("Capturing", frame)
-        key = cv2.waitKey(1)
-        for i in range(10):
-            name='saved_img%s.jpg' %i
-            cv2.imwrite(filename=name, img=frame)
-            image = face_recognition.load_image_file(name)
-            face_locations = face_recognition.face_locations(image)
-            
-            time.sleep(3)
+#        print(check) #prints true as long as the webcam is running
+#        print(frame) #prints matrix values of each framecd 
+        
+        #cv2.imwrite(filename=name, img=frame)
+        #image = face_recognition.load_image_file(name)
+        image = frame
+        
+        face_locations = face_recognition.face_locations(image)
+        for face in face_locations:
+            cv2.rectangle(frame, (face[3], face[0]), (face[1], face[2]), (0, 0, 255), 2)
 
+
+        print(face_locations.__len__())
+        cv2.imshow("Capturing", frame)
+
+        print(face_locations)           
+        key = cv2.pollKey()
         if key == ord('q'):
             print("Turning off camera.")
             webcam.release()
